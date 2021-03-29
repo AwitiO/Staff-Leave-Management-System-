b5465e2c032c0adec6419e7d17093b5509fbf8d6
AwitiO <73213781+AwitiO@users.noreply.github.com>
2021-03-29 23:28:50 +0300

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Styles/dashboard.css">
+  <script src="https://kit.fontawesome.com/0e62e3bdc9.js" crossorigin="anonymous"></script>
+  <title>Dashboard</title>
+</head>
+<body>
+  
+<section id="sidebar">
+<div class="links">
+<div class="icons">
+<i class=""></i>
+</div>
+
+</div>
+</section>
+<div class="main-content">
+Dashboard
+</div>
+
+</body>
+</html>
